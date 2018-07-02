f85d53b0c1c2a508ba0ab065f0afade275c64836
ernestvw <evw0301@gmail.com>
2018-07-02 17:26:12 +0200

    Update maxBipartiteMatch.cpp

--- Graphs/Flows Cuts and Matchings/maxBipartiteMatch.cpp
+++ Graphs/Flows Cuts and Matchings/maxBipartiteMatch.cpp
@@ -1,62 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
+int V;
+vector<int> adj[1001];
+bool seen[1001];
+int match[1001];
 
-const int MAX_V = (int)1e3+5;
-const int MAX_E = (int)1e6+5;
-
-int V,E;
-bool couple[MAX_V];
-bool utilise[MAX_V][MAX_V];
-bool vu[MAX_V];
-vector<int> adj[MAX_V];
-
-bool coupler(int u,bool t) {
-    if(!couple[u] && t) return true;
-    if(!vu[u]) {
-        vu[u]=true;
-        for(int i=0;i<(int)adj[u].size();i++) {
-            int v=adj[u][i];
-            if(utilise[u][v]==t && coupler(v,!t)) {
-                couple[u]=true;
-                couple[v]=true;
-                utilise[u][v]=!t;
-                return true;
-            }
-        }
-    }
-    return false;
+bool aug(int u) {
+	if(seen[u]) return 0;
+	seen[u] = 1;
+	for(int v : adj[u]) {
+		if(match[v] == -1 or aug(match[v])) {
+			match[v] = u;
+			return 1;
+		}
+	}
+	return 0;
 }
 
-int matching() {
-    bool maximal=false;
-    while(!maximal) {
-        maximal=true;
-        for(int i=0;i<V;i++) vu[i]=false;
-        for(int u=0;u<V;u++)
-            if(!couple[u] && coupler(u,false))
-                maximal=false;
-    }
-    int nbMatch=0;
-    for(int i=0;i<V;i++)
-        if(couple[i])
-            nbMatch++;
-    return nbMatch;
-}
-
-int main()
-{
-    scanf("%d%d",&V,&E);
-    for(int i=0;i<V;i++) {
-        couple[i]=false;
-        vu[i]=false;
-        for(int j=0;j<V;j++) utilise[i][j]=false;
-    }
-    for(int i=0;i<E;i++) {
-        int u,v;
-        scanf("%d%d",&u,&v);
-        adj[u].push_back(v);
-    }
-    cout<<matching()<<endl;
-    
-    return 0;
+int MCBM() {
+	int mcbm = 0;
+	fill_n(match, V+1, -1);
+	for(int i = 0; i < V; i++) {
+		fill_n(seen, V+1, 0);
+		mcbm += aug(i);
+	}
+	return mcbm;
 }
